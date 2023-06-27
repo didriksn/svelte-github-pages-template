6da4be2c09f094347602b88b1d32bf42cd36b32b
didrik <didrik.sommersten@gmail.com>
2023-06-27 23:24:17 +0200

    e

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/3svelteghpages/",
+  base: "/svelte-github-pages-template/",
   plugins: [svelte()]
 })
